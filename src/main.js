133abdbd52b4eb5523c4c3c59cc1a7e0d9f2c1a4
Fajar Patappari <top.sick.red@gmail.com>
2018-02-21 19:08:59 +0700

    change API port

--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ import App from './components/App'
 import FBSignInButton from 'vue-facebook-signin-button'
 
 Vue.config.productionTip = false
-axios.defaults.baseURL = 'http://api.fajarpatappari.tk:3000/api';
+axios.defaults.baseURL = 'http://api.fajarpatappari.tk:3003/api';
 // axios.defaults.baseURL = 'http://localhost:3000/api';
 axios.defaults.headers.common['Authorization'] = localStorage.getItem('token')
 
